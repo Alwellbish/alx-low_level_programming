ef1a8a73cdf2c46c469ae0c5079bb87bbf9174dc
ALWELL <alwellbish@gmail.com>
2022-09-12 16:34:04 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-extern size_t fwrite("and that piece of art is useful"
+  extern size_t fwrite(STDOUT_FILENO, "and that piece of art is useful"
 " - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
