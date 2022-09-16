dac3181761771e7a2f5e74bb6d3e7de00f48b6ae
ALWELL <alwellbish@gmail.com>
2022-09-16 08:44:40 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
- * jack_bauer - Check every minute of the day of Jack Bauer, starting from 00:00 to 23:59
+ * jack_bauer - Check every minute of
+ * the Jack Bauer
  * Description: function prints every minute in one day
  * Return: Nothing
  */
