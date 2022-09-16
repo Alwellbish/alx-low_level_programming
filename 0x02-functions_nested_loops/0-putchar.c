57fc3f41daf95ad021b2c1db4d74b117ea988a42
ALWELL <alwellbish@gmail.com>
2022-09-16 06:45:24 -0700

    Main h correction

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,64 @@
+
+Document (86).docx
+
+1 / 11
+Main.h below copy it  
+
+#ifndef HOLBERTON_H_INCLUDED
+#define HOLBERTON_H_INCLUDED
+
+Int _putchar(char);
+Void print_alphabet(void);
+Void print_alphabet_x10(void);
+Int _islower(int c);
+Int _isalpha(int c);
+Int print_sign(int n);
+Int _abs(int);
+Int print_last_digit(int);
+Void jack_bauer(void);
+Void times_table(void);
+Int add(int a, int b);
+Void print_to_98(int n);
+Void print_times_table(int n);
+Void num(int r, int n);
+
+#endif
+
+_putchar copy it below
+
+#include <unistd.h>
+
+/**
+ * _putchar – writes the character c to stdout
+ * @c: The character to print
+2 / 11
+*
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
+*/
+Int _putchar(char c)
+{
+  Return (write(1, &c, 1));
+}
 #include "main.h"
 
 /**
- * main - entry point
- * _putchar: The character to print
- *
- * Return: On success 0.
- * 
+ * main - print putchar
+ * Return: always 0
  */
+
 int main(void)
 {
-char i = "_putchar"
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 
-for (i=1; i<=0; i++)
-{
-putchar ('i');
-}
-putchar ('\n');
+return (0);
 
-return 0;
 }
